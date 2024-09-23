8b4a03ba4b16c700932fbfe0d5258215f3121c65
Christopher Todd <christophertodd0509@icloud.com>
2024-09-23 09:50:06 -0500

    bound player movement to the screen

---
+++ Assets/Scripts/Game/Constants.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants : MonoBehaviour
+{
+    public const float LEFTBORDER = -38.47f;
+    public const float RIGHTBORDER = 38.47f;
+    public const float TOPBORDER = 23.33f;
+    public const float BOTTOMBORDER = -15.48f;
+}
--- Assets/Scripts/Game/Player/PlayerMovement.cs
+++ Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.InputSystem; // Allows for input to be read
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,20 +11,57 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _movementSpeed;
     [SerializeField] private float _acceleration;
     private Vector2 _velocity = Vector2.zero;
+    private Camera _camera;
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
     private void FixedUpdate()
     {
         _velocity = Vector2.Lerp(_velocity, _movementInput * _movementSpeed, _acceleration);
+        ClampMovementToScreenSize();
         _rigidbody.velocity = _velocity;
     }
-    private void OnMove(InputValue inputValue) // Relies on UnityEngine.InputSystem
+    private void OnMove(InputValue inputValue)
     {
         Vector2 inputVector = inputValue.Get<Vector2>();
         _movementInput = inputVector;
-        if (inputVector.x > 0)
+        FlipScaleToXInput(inputVector.x);
+    }
+    private void ClampMovementToScreenSize()
+    {
+        if (transform.position.x < Constants.LEFTBORDER)
+        {
+            if (_rigidbody.velocity.x < 0)
+                _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
+            transform.position = new Vector2(Constants.LEFTBORDER, transform.position.y);
+        }
+        if (transform.position.x > Constants.RIGHTBORDER)
+        {
+            if (_rigidbody.velocity.x > 0)
+                _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
+            transform.position = new Vector2(Constants.RIGHTBORDER, transform.position.y);
+        }
+        if (transform.position.y < Constants.BOTTOMBORDER)
+        {
+            if (_rigidbody.velocity.y < 0)
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0);
+            transform.position = new Vector2(transform.position.x, Constants.BOTTOMBORDER);
+        }
+        if (transform.position.y > Constants.TOPBORDER)
+        {
+            if (_rigidbody.velocity.y > 0)
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0);
+            transform.position = new Vector2(transform.position.x, Constants.TOPBORDER);
+        }
+    }
+    private void FlipScaleToXInput(float xInput)
+    {
+        if (xInput > 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
         }
-        else if (inputVector.x < 0)
+        else if (xInput < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
