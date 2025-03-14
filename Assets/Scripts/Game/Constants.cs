1ecc59e0769427b068ad3b68cd2d0afe19c13943
Christopher Todd <christophertodd0509@icloud.com>
2025-03-14 11:12:29 -0500

    increased arena size

--- Assets/Scripts/Game/Constants.cs
+++ Assets/Scripts/Game/Constants.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Constants : MonoBehaviour
 {
-    public const float LEFTBORDER = -38.47f;
-    public const float RIGHTBORDER = 38.47f;
-    public const float TOPBORDER = 23.33f;
-    public const float BOTTOMBORDER = -15.48f;
+    public const float LEFTBORDER = -56.59f;
+    public const float RIGHTBORDER = 56.59f;
+    public const float TOPBORDER = 33.94f;
+    public const float BOTTOMBORDER = -25.47f;
 }
